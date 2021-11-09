478b94e331a7aac48a252eabc66bfe52766fc824
ItsAmex <proames75yt@gmail.com>
2021-11-09 18:39:42 -0500

    uwu

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import app from './app.vue'
-
-import router from './util/pages'
-
-const project = createApp(app)
-project.use(router)
-project.mount('#app')
--- src/pages/buy.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <br><br><br><br><br><br>Soon...
-</template>
